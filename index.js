237554dfcd879308801d6bb46ea18b60860dd9eb
Carlos Eduardo <kaduinfo@gmail.com>
2020-09-11 19:57:27 -0300

    Post Request with fetch

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const app = express()
 app.listen(3000, () => console.log('start server'))
 
 app.use(express.static('public'))
+app.use(express.json({ limit: '1mb' }))
+
+app.post('/api', (resquest, response) => {
+    console.log(resquest)
+    const data = resquest.body
+    response.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lon
+    })
+})
